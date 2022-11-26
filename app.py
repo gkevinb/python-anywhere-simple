d613cd1abdf6c913a2a89851b706a135554eac1b
Gabor Kevin Barta <gkevinba@gmail.com>
2022-11-26 21:55:42 +0100

    minor change

--- app.py
+++ app.py
@@ -12,7 +12,6 @@ def hello_world():
 def test():
     return "<p>Testing new route endpoint!</p>"
 
-
 @app.route("/git-update", methods=["POST"])
 def git_update():
     path = pathlib.Path(__file__).parent.resolve()
